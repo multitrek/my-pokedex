ccc4ee3ff1fa5db67bdd54bbda63e6e34757d6ae
pecabralsantos <pecabralsantos@gmail.com>
2024-01-21 09:50:59 -0300

    COnfiguração inicial do koin

---
+++ feature-pokemon/src/main/java/br/com/multitrek/feature/pokemon/MainApplication.kt
@@ -0,0 +1,20 @@
+package br.com.multitrek.feature.pokemon
+
+import android.app.Application
+import br.com.multitrek.feature.pokemon.di.pokemonModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@MainApplication)
+            modules(pokemonModule)
+        }
+    }
+}
---
+++ feature-pokemon/src/main/java/br/com/multitrek/feature/pokemon/di/pokemonModule.kt
@@ -0,0 +1,5 @@
+package br.com.multitrek.feature.pokemon.di
+
+import org.koin.dsl.module
+
+val pokemonModule = module {  }
